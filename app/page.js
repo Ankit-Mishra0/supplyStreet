eae9da931550e5afbb574c1228df3ae473d0283a
Ankit Mishra <ankitsocial1103@gmail.com>
2025-07-26 15:13:20 +0530

    button added

--- app/page.js
+++ app/page.js
@@ -5,6 +5,7 @@ export default function Home() {
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
       <h1 className="text-4xl font-bold mb-6 text-red-500">Welcome to  My App</h1>
       <Link href="/login"> <button>login</button></Link>
+       <Link href="/dashboard"> <button>dashboard</button></Link>
      
     </div>
   );
